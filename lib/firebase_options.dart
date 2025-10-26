b8cd992d7a43e44d8964e78236d058153c8960af
minj-03 <133262575+minj-03@users.noreply.github.com>
2025-10-26 20:48:22 +0530

    Replace Firebase project values with placeholder
    Replaced Firebase configuration values with a placeholder.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -6,13 +6,7 @@ import 'package:flutter/foundation.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-      apiKey: "AIzaSyDZ0S2OlASGnb5KqS1QV94rxvvndNNxxxg",
-      authDomain: "scatterout-2e776.firebaseapp.com",
-      projectId: "scatterout-2e776",
-      storageBucket: "scatterout-2e776.firebasestorage.app",
-      messagingSenderId: "542739404245",
-      appId: "1:542739404245:web:d8746e2fdecbd48e3d995c",
-      measurementId: "G-E051E3C5XV",
+      PLACEHOLDER_CONFIGURATIONS_FIREBASE_PROJECT
     );
   }
 }
